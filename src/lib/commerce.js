adb027bd9ec8060faaf9089c3938a66702f31828
sumit singh <76992713+sumitsingh4411@users.noreply.github.com>
2021-01-24 10:02:10 +0530

    update commercejs api_key

--- src/lib/commerce.js
+++ src/lib/commerce.js
@@ -1,3 +1,3 @@
 import Commerce from '@chec/commerce.js';
 
-export const commerce = new Commerce('pk_test_222138a865d5d9de1b1938cf6d60d5a57ed243ffdfaa4');
+export const commerce = new Commerce('enter your key');
